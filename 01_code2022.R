4688f4ff19e504f26350cdacb5f79014c8934f86
rgerecke <renatavgerecke@gmail.com>
2022-12-02 23:22:34 -0500

    day 2 complete

--- 01_code2022.R
+++ 01_code2022.R
@@ -27,3 +27,63 @@ df |>
   summarise(a = sum(a))
 
 ## 205370
+
+
+# day 2 ---------
+
+df <- read_csv("data/2022/day02.csv",
+               col_names = "a")
+
+rps <- function(opp,player) {
+  pt1 <- case_when(
+    opp == "A" & player == "X" ~ 3,
+    opp == "B" & player == "Y" ~ 3,
+    opp == "C" & player == "Z" ~ 3,
+    opp == "A" & player == "Y" ~ 6,
+    opp == "B" & player == "Z" ~ 6,
+    opp == "C" & player == "X" ~ 6,
+    TRUE ~ 0
+  )
+
+  pt2 <- case_when(
+    player == "X" ~ 1,
+    player == "Y" ~ 2,
+    player == "Z" ~ 3
+  )
+
+  return(pt1 + pt2)
+}
+
+
+df |>
+  separate(a, c("a", "b"), sep = " ") |>
+  mutate(
+    c = map2_dbl(a, b, rps)
+  ) |>
+  summarise(sum(c))
+
+## 13565
+
+## part 2 -------------
+
+rps2 <- function(opp, out) {
+  opp_num <- match(opp, LETTERS) - 1
+
+  play_num <- case_when(
+    out == "X" ~ (opp_num - 1) %% 3,
+    out == "Y" ~ opp_num,
+    out == "Z" ~ (opp_num + 1) %% 3,
+  )
+
+  play <- LETTERS[play_num + 24]
+
+  return(rps(opp, play))
+}
+
+
+df |>
+  separate(a, c("a", "b"), sep = " ") |>
+  mutate(c = map2_dbl(a, b, rps2)) |>
+  summarise(sum(c))
+
+## 12424
